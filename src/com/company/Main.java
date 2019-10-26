ca76566cc83e8070654123212973fa0fa2f60f26
Kasia <superkasia3@gmail.com>
2019-10-26 14:59:47 +0200

    wizytowkaxD

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,6 +4,10 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        System.out.println("aasdaDSDAD");
+        System.out.println("_______________");
+        System.out.println("Katarzyna blablal");
+        System.out.println("bla ba bla alb");
+        System.out.println("hasdhjasd kasdka");
+        System.out.println("________________");
     }
 }
